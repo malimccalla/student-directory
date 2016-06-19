64e25e188f5058143edeefe93b0fe8f71a193f13
Mali Michael <malimccalla@gmail.com>
2016-06-19 20:18:38 +0100

    Add a list of students

---
+++ directory.rb
@@ -0,0 +1,14 @@
+puts "-"*50
+puts "The students of Villains Academy".center(50)
+puts "-"*50
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex Delarge"
+puts "The Wicked Witch Of The West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"

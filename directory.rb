4f26c6c0373ea67da9c88bd49c600e727edfb88f
Mali Michael <malimccalla@gmail.com>
2016-06-20 11:36:23 +0100

    Add students to a Hash foor readability

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #first we print the list of students
 students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex Delarge",
- "The Wicked Witch Of The West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates",
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex Delarge", cohort: :november},
+ {name: "The Wicked Witch Of The West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddy Krueger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Joffrey Baratheon", cohort: :november},
+ {name: "Norman Bates", cohort: :november},
 ]
 def print_header
   puts "-"*50
@@ -18,12 +18,14 @@ def print_header
   puts "-"*50
 end
 
-def print_names(names)
-  names.each { |name| puts name }
+def print_names(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.length} great students"
+def print_footer(students)
+  puts "Overall we have #{students.length} great students"
 end
 
 print_header

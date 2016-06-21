853f9eede9a45784440ba2829ebed9e3f19ff9aa
Mali Michael <malimccalla@gmail.com>
2016-06-21 10:41:16 +0100

    Ask for more info about students for directory

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
 #first we print the list of students
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex Delarge", cohort: :november},
- {name: "The Wicked Witch Of The West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Krueger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name: "Norman Bates", cohort: :november},
-]
+
+# students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name: "Darth Vader", cohort: :november},
+#  {name: "Nurse Ratched", cohort: :november},
+#  {name: "Michael Corleone", cohort: :november},
+#  {name: "Alex Delarge", cohort: :november},
+#  {name: "The Wicked Witch Of The West", cohort: :november},
+#  {name: "Terminator", cohort: :november},
+#  {name: "Freddy Krueger", cohort: :november},
+#  {name: "The Joker", cohort: :november},
+#  {name: "Joffrey Baratheon", cohort: :november},
+#  {name: "Norman Bates", cohort: :november},
+# ]
 
 def input_students
   puts "Please enter the names of the students"
@@ -19,8 +20,21 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "Please enter the students cohort"
+    cohort = gets.chomp
+    while cohort.empty? do
+      puts "You must enter a cohort: "
+      cohort = gets.chomp
+    end
+    puts "Please enter the students birth country"
+    country = gets.chomp
+    while country.empty? do
+      puts "You must enter a country of birth"
+      country = gets.chomp
+    end
+    students << {name: name, cohort: cohort, country: country}
     puts "Now we have #{students.count} students"
+    puts "Please enter next students name or hit enter to end."
     name = gets.chomp
   end
   students
@@ -35,7 +49,7 @@ end
 def print_names(students)
   index = 0
   while index < students.length
-    puts "#{index+1}.#{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    puts "#{index+1}.#{students[index][:name]} (#{students[index][:cohort]}). Country of birth: #{students[index][:country]}"
     index += 1
   end
 end

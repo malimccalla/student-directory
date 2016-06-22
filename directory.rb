4c836551990c50c4f8c6f4151c718f25db128ab4
Mali Michael <malimccalla@gmail.com>
2016-06-22 11:44:57 +0100

    Create a save method to save into 'students.csv'

--- directory.rb
+++ directory.rb
@@ -63,12 +63,13 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print_students_list#(students)
+  print_names#(students)
   print_footer#(students)
 end
 
@@ -86,6 +87,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -101,7 +104,7 @@ def print_header
   puts "-"*50
 end
 
-def print_students_list#(students)
+def print_names#(students)
   cohort_sorted = @students.group_by {|e| e[:cohort]}
   cohort_sorted.each do |k,v|
     puts '-'*50
@@ -116,6 +119,16 @@ end
 
 def print_footer#(students)
   puts "Overall we #{@students.length} great students"
+end
+
+def save_students
+  file = File.open('students.csv', 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts(csv_line)
+  end
+  file.close
 end
 
 interactive_menu
